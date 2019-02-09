d4c064aa241b0136e72f47e91d4735e49db82d68
Arkator7 <Arkator7@gmail.com>
2019-02-09 16:41:38 +1100

    Set up initial tests from specs

---
+++ src/WerkDaze.Api/BusinessDayCounter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace WerkDaze.Api
+{
+    public class BusinessDayCounter
+    {
+        public int WeekdaysBetweenTwoDates(DateTime firstDate, DateTime secondDate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int BusinessDaysBetweenTwoDates(DateTime firstDate, DateTime secondDate)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ test/WerkDaze.UnitTests/BusinessDayCounterTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using WerkDaze.Api;
+using Xunit;
+
+namespace WerkDaze.UnitTests
+{
+    public class BusinessDayCounterTests
+    {
+        [Fact]
+        public void WeekdaysBetweenTwoDates_SampleData_ReturnCorrectValue()
+        {
+            BusinessDayCounter bdc = new BusinessDayCounter();
+
+            Assert.Equal(1, bdc.WeekdaysBetweenTwoDates(new DateTime(2013, 10, 7), new DateTime(2013, 10, 9)));
+            Assert.Equal(5, bdc.WeekdaysBetweenTwoDates(new DateTime(2013, 10, 5), new DateTime(2013, 10, 14)));
+            Assert.Equal(61, bdc.WeekdaysBetweenTwoDates(new DateTime(2013, 10, 7), new DateTime(2014, 1, 1)));
+            Assert.Equal(0, bdc.WeekdaysBetweenTwoDates(new DateTime(2013, 10, 7), new DateTime(2013, 10, 5)));
+        }
+
+        [Fact]
+        public void BusinessDaysBetweenTwoDates_SampleData_ReturnCorrectValue()
+        {
+            BusinessDayCounter bdc = new BusinessDayCounter();
+
+            Assert.Equal(1, bdc.BusinessDaysBetweenTwoDates(new DateTime(2013, 10, 7), new DateTime(2013, 10, 9)));
+            Assert.Equal(0, bdc.BusinessDaysBetweenTwoDates(new DateTime(2013, 12, 24), new DateTime(2013, 12, 27)));
+            Assert.Equal(59, bdc.BusinessDaysBetweenTwoDates(new DateTime(2013, 10, 7), new DateTime(2014, 1, 1)));
+        }
+    }
+}
