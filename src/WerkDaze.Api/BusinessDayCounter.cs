eefd4fdf0c0d13cc518f67362473b577e07831ca
Arkator7 <Arkator7@gmail.com>
2019-02-10 14:31:57 +1100

    Experiment: Explore data structures, made functions static

--- src/WerkDaze.Api/BusinessDayCounter.cs
+++ src/WerkDaze.Api/BusinessDayCounter.cs
@@ -4,12 +4,20 @@ namespace WerkDaze.Api
 {
     public class BusinessDayCounter
     {
-        public int WeekdaysBetweenTwoDates(DateTime firstDate, DateTime secondDate)
+        public static int WeekdaysBetweenTwoDates(DateTime firstDate, DateTime secondDate)
         {
-            throw new NotImplementedException();
+            DateTimeOffset dto1 = new DateTimeOffset(firstDate);
+            DateTimeOffset dto2 = new DateTimeOffset(secondDate);
+
+            TimeSpan difference = dto2 - dto1;
+
+            return Convert.ToInt32(Math.Round(difference.TotalDays)) - 1;
+
+
+            //throw new NotImplementedException();
         }
 
-        public int BusinessDaysBetweenTwoDates(DateTime firstDate, DateTime secondDate)
+        public static int BusinessDaysBetweenTwoDates(DateTime firstDate, DateTime secondDate)
         {
             throw new NotImplementedException();
         }
--- test/WerkDaze.UnitTests/BusinessDayCounterTests.cs
+++ test/WerkDaze.UnitTests/BusinessDayCounterTests.cs
@@ -9,22 +9,20 @@ namespace WerkDaze.UnitTests
         [Fact]
         public void WeekdaysBetweenTwoDates_SampleData_ReturnCorrectValue()
         {
-            BusinessDayCounter bdc = new BusinessDayCounter();
+            //BusinessDayCounter bdc = new BusinessDayCounter();
 
-            Assert.Equal(1, bdc.WeekdaysBetweenTwoDates(new DateTime(2013, 10, 7), new DateTime(2013, 10, 9)));
-            Assert.Equal(5, bdc.WeekdaysBetweenTwoDates(new DateTime(2013, 10, 5), new DateTime(2013, 10, 14)));
-            Assert.Equal(61, bdc.WeekdaysBetweenTwoDates(new DateTime(2013, 10, 7), new DateTime(2014, 1, 1)));
-            Assert.Equal(0, bdc.WeekdaysBetweenTwoDates(new DateTime(2013, 10, 7), new DateTime(2013, 10, 5)));
+            Assert.Equal(1, BusinessDayCounter.WeekdaysBetweenTwoDates(new DateTime(2013, 10, 7), new DateTime(2013, 10, 9)));
+            Assert.Equal(5, BusinessDayCounter.WeekdaysBetweenTwoDates(new DateTime(2013, 10, 5), new DateTime(2013, 10, 14)));
+            Assert.Equal(61, BusinessDayCounter.WeekdaysBetweenTwoDates(new DateTime(2013, 10, 7), new DateTime(2014, 1, 1)));
+            Assert.Equal(0, BusinessDayCounter.WeekdaysBetweenTwoDates(new DateTime(2013, 10, 7), new DateTime(2013, 10, 5)));
         }
 
         [Fact]
         public void BusinessDaysBetweenTwoDates_SampleData_ReturnCorrectValue()
         {
-            BusinessDayCounter bdc = new BusinessDayCounter();
-
-            Assert.Equal(1, bdc.BusinessDaysBetweenTwoDates(new DateTime(2013, 10, 7), new DateTime(2013, 10, 9)));
-            Assert.Equal(0, bdc.BusinessDaysBetweenTwoDates(new DateTime(2013, 12, 24), new DateTime(2013, 12, 27)));
-            Assert.Equal(59, bdc.BusinessDaysBetweenTwoDates(new DateTime(2013, 10, 7), new DateTime(2014, 1, 1)));
+            Assert.Equal(1, BusinessDayCounter.BusinessDaysBetweenTwoDates(new DateTime(2013, 10, 7), new DateTime(2013, 10, 9)));
+            Assert.Equal(0, BusinessDayCounter.BusinessDaysBetweenTwoDates(new DateTime(2013, 12, 24), new DateTime(2013, 12, 27)));
+            Assert.Equal(59, BusinessDayCounter.BusinessDaysBetweenTwoDates(new DateTime(2013, 10, 7), new DateTime(2014, 1, 1)));
         }
     }
 }
