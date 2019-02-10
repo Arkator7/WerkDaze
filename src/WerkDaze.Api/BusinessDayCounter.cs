34d7964cd629345ab54f674a0e4e6897de004e35
Arkator7 <Arkator7@gmail.com>
2019-02-11 01:05:10 +1100

    Task 1: Resolve Weekend Work Calculator using modulus and tools

--- src/WerkDaze.Api/BusinessDayCounter.cs
+++ src/WerkDaze.Api/BusinessDayCounter.cs
@@ -1,25 +1,178 @@
 ﻿using System;
+using System.Runtime.CompilerServices;
 
+[assembly: InternalsVisibleTo("WerkDaze.UnitTests")]
 namespace WerkDaze.Api
 {
     public class BusinessDayCounter
     {
+        public static int STD_DAY_IN_YEAR = 365;
+        public static int[] ACCUM_DAYS_FOR_MTHS = new int[] { 0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334 };
+        public static int LEAP = 4;
+        public static int CENTURY = 100;
+
+        public static int MONTHS_IN_YEAR = 12;
+
+        public static int WEEK = 7;
+        public static int WORK_WEEK = 5;
+
         public static int WeekdaysBetweenTwoDates(DateTime firstDate, DateTime secondDate)
         {
-            DateTimeOffset dto1 = new DateTimeOffset(firstDate);
-            DateTimeOffset dto2 = new DateTimeOffset(secondDate);
+            int dto1 = GetDayHash(firstDate);
+            int dto2 = GetDayHash(secondDate);
 
-            TimeSpan difference = dto2 - dto1;
+            int difference = dto2 - dto1;
 
-            return Convert.ToInt32(Math.Round(difference.TotalDays)) - 1;
+            // Quick Fail
+            if (difference <= 0)
+            {
+                return 0;
+            }
 
+            // Shave off weeks
+            if (difference >= WEEK)
+            {
+                int weeks = difference / WEEK;
+                //int modulus = difference % WEEK;
 
-            //throw new NotImplementedException();
+                dto1 += weeks * WEEK;
+
+                return (weeks * WORK_WEEK) + WeekdaysBetweenTwoDates(ReverseDayHash(dto1), secondDate);
+            }
+
+            int workDays = 0;
+
+            // Loop Exclusive to end date
+            while (dto1 < dto2 - 1)
+            {
+                DateTime d1 = ReverseDayHash(dto1);
+
+                if (!IsWeekend(d1))
+                {
+                    workDays += 1;
+                }
+
+                dto1 += 1;
+
+                d1 = ReverseDayHash(dto1);
+            }
+
+            return workDays;
+        }
+
+        /// <summary>
+        /// Assign a value to each date, starting with 1/1/0001 (being 0) to 31/12/9999. 
+        /// As Day of the Week is consistent (7 days a week), you can simply use modulus
+        /// to determine day.
+        /// </summary>
+        /// <param name="date"></param>
+        /// <returns></returns>
+        internal static int GetDayHash(DateTime date)
+        {
+            int year = date.Year - 1;
+            int month = date.Month - 1;
+            int day = date.Day - 1;
+
+            int leapDays = (year / LEAP) - (year / CENTURY) + (year / (LEAP * CENTURY));
+
+            //If month is beyond Feb and is a leap year
+            if (month > 1 && IsLeapYear(date))
+            {
+                leapDays += 1;
+            }
+
+            return (year * STD_DAY_IN_YEAR) + leapDays + ACCUM_DAYS_FOR_MTHS[month] + day;
+        }
+
+        internal static DateTime ReverseDayHash(int hash)
+        {
+            int approx_year = hash / STD_DAY_IN_YEAR - 1;
+
+            DateTime dt = new DateTime(approx_year, 1, 1);
+            int approx_hash = GetDayHash(dt);
+
+            int difference = hash - approx_hash;
+
+            // Self-correction
+            while (difference > STD_DAY_IN_YEAR)
+            {
+                approx_year += 1;
+
+                dt = new DateTime(approx_year, 1, 1);
+                approx_hash = GetDayHash(dt);
+
+                difference = hash - approx_hash;
+            }
+
+            int month_index = MONTHS_IN_YEAR - 1;
+            dt = new DateTime(approx_year, month_index + 1, 1);
+            approx_hash = GetDayHash(dt);
+
+            while ((difference - ACCUM_DAYS_FOR_MTHS[month_index]) < 0)
+            {
+                month_index -= 1;
+
+                dt = new DateTime(approx_year, month_index + 1, 1);
+                approx_hash = GetDayHash(dt);
+            }
+
+            difference = hash - approx_hash;
+
+            dt = new DateTime(approx_year, month_index + 1, difference + 1);
+
+            return dt;
+        }
+
+        private static bool IsLeapYear(DateTime date)
+        {
+            if (date.Year % 4 != 0)
+            {
+                return false;
+            } else if (date.Year % 100 != 0)
+            {
+                return true;
+            } else if (date.Year % 400 != 0)
+            {
+                return false;
+            } else
+            {
+                return true;
+            }
+        }
+
+        /// <summary>
+        /// Returns modulus of GetHashDay()
+        /// </summary>
+        /// <param name="date"></param>
+        /// <returns></returns>
+        internal static int GetDay(DateTime date)
+        {
+            return GetDayHash(date) % 7;
+        }
+
+        /// <summary>
+        /// Determine if day is weekend
+        /// 
+        /// Why Our Calendars Skipped 11 Days in 1752
+        /// http://mentalfloss.com/article/51370/why-our-calendars-skipped-11-days-1752
+        /// Old 0 = Friday
+        /// New 0 = Monday
+        /// 
+        /// 
+        /// </summary>
+        /// <param name="day"></param>
+        /// <returns></returns>
+        internal static bool IsWeekend(DateTime date)
+        {
+            return ((date < new DateTime(1752, 9, 2) && (GetDay(date) == 1 || GetDay(date) == 2)) ||
+                (date > new DateTime(1752, 9, 14) && (GetDay(date) == 5 || GetDay(date) == 6)));
         }
 
         public static int BusinessDaysBetweenTwoDates(DateTime firstDate, DateTime secondDate)
         {
             throw new NotImplementedException();
         }
+
+
     }
 }
--- test/WerkDaze.UnitTests/BusinessDayCounterTests.cs
+++ test/WerkDaze.UnitTests/BusinessDayCounterTests.cs
@@ -9,8 +9,6 @@ namespace WerkDaze.UnitTests
         [Fact]
         public void WeekdaysBetweenTwoDates_SampleData_ReturnCorrectValue()
         {
-            //BusinessDayCounter bdc = new BusinessDayCounter();
-
             Assert.Equal(1, BusinessDayCounter.WeekdaysBetweenTwoDates(new DateTime(2013, 10, 7), new DateTime(2013, 10, 9)));
             Assert.Equal(5, BusinessDayCounter.WeekdaysBetweenTwoDates(new DateTime(2013, 10, 5), new DateTime(2013, 10, 14)));
             Assert.Equal(61, BusinessDayCounter.WeekdaysBetweenTwoDates(new DateTime(2013, 10, 7), new DateTime(2014, 1, 1)));
@@ -23,6 +21,137 @@ namespace WerkDaze.UnitTests
             Assert.Equal(1, BusinessDayCounter.BusinessDaysBetweenTwoDates(new DateTime(2013, 10, 7), new DateTime(2013, 10, 9)));
             Assert.Equal(0, BusinessDayCounter.BusinessDaysBetweenTwoDates(new DateTime(2013, 12, 24), new DateTime(2013, 12, 27)));
             Assert.Equal(59, BusinessDayCounter.BusinessDaysBetweenTwoDates(new DateTime(2013, 10, 7), new DateTime(2014, 1, 1)));
+        }
+
+        [Fact]
+        public void GetHashDay_SampleData_ReturnsCorrectValue()
+        {
+            Assert.Equal(0, BusinessDayCounter.GetDayHash(new DateTime(1, 1, 1)));
+            Assert.Equal(1, BusinessDayCounter.GetDayHash(new DateTime(1, 1, 2)));
+            Assert.Equal(31, BusinessDayCounter.GetDayHash(new DateTime(1, 2, 1)));
+            Assert.Equal(59, BusinessDayCounter.GetDayHash(new DateTime(1, 3, 1)));
+            Assert.Equal(365, BusinessDayCounter.GetDayHash(new DateTime(2, 1, 1)));
+            Assert.Equal(730, BusinessDayCounter.GetDayHash(new DateTime(3, 1, 1)));
+            Assert.Equal(1461, BusinessDayCounter.GetDayHash(new DateTime(5, 1, 1)));
+            Assert.Equal(3652, BusinessDayCounter.GetDayHash(new DateTime(11, 1, 1)));
+            Assert.Equal(36524, BusinessDayCounter.GetDayHash(new DateTime(101, 1, 1)));
+            Assert.Equal(146097, BusinessDayCounter.GetDayHash(new DateTime(401, 1, 1)));
+            Assert.Equal(584388, BusinessDayCounter.GetDayHash(new DateTime(1601, 1, 1)));
+        }
+
+        [Fact]
+        public void GetHashDay_SampleData_1752EdgeCase_ReturnsCorrectValue()
+        {
+            Assert.Equal(639539, BusinessDayCounter.GetDayHash(new DateTime(1752, 1, 1)));
+            Assert.Equal(639598, BusinessDayCounter.GetDayHash(new DateTime(1752, 2, 29)));
+            Assert.Equal(639599, BusinessDayCounter.GetDayHash(new DateTime(1752, 3, 1)));
+            Assert.Equal(639609, BusinessDayCounter.GetDayHash(new DateTime(1752, 3, 11)));
+            Assert.Equal(639619, BusinessDayCounter.GetDayHash(new DateTime(1752, 3, 21)));
+            Assert.Equal(639629, BusinessDayCounter.GetDayHash(new DateTime(1752, 3, 31)));
+            Assert.Equal(639630, BusinessDayCounter.GetDayHash(new DateTime(1752, 4, 1)));
+            Assert.Equal(639904, BusinessDayCounter.GetDayHash(new DateTime(1752, 12, 31)));
+            Assert.Equal(639905, BusinessDayCounter.GetDayHash(new DateTime(1753, 1, 1)));
+        }
+
+        [Fact]
+        public void GetHashDay_SampleData_ModernDates_ReturnsCorrectValue()
+        {
+            Assert.Equal(652713, BusinessDayCounter.GetDayHash(new DateTime(1788, 1, 26)));
+
+            Assert.Equal(730485, BusinessDayCounter.GetDayHash(new DateTime(2001, 1, 1)));
+            Assert.Equal(731946, BusinessDayCounter.GetDayHash(new DateTime(2005, 1, 1)));
+            Assert.Equal(733407, BusinessDayCounter.GetDayHash(new DateTime(2009, 1, 1)));
+            Assert.Equal(734868, BusinessDayCounter.GetDayHash(new DateTime(2013, 1, 1)));
+        }
+
+        [Fact]
+        public void GetHashDay_SampleData_LeapYearTest_ReturnsCorrectValue()
+        {
+            Assert.Equal(735963, BusinessDayCounter.GetDayHash(new DateTime(2016, 1, 1)));
+            Assert.Equal(735994, BusinessDayCounter.GetDayHash(new DateTime(2016, 2, 1)));
+            Assert.Equal(736022, BusinessDayCounter.GetDayHash(new DateTime(2016, 2, 29)));
+            Assert.Equal(736023, BusinessDayCounter.GetDayHash(new DateTime(2016, 3, 1)));
+        }
+
+        [Fact]
+        public void GetHashDay_SampleData_2019Test_ReturnsCorrectValue()
+        {
+            Assert.Equal(737059, BusinessDayCounter.GetDayHash(new DateTime(2019, 1, 1)));
+            Assert.Equal(737090, BusinessDayCounter.GetDayHash(new DateTime(2019, 2, 1)));
+            Assert.Equal(737099, BusinessDayCounter.GetDayHash(new DateTime(2019, 2, 10)));
+        }
+
+        [Fact]
+        public void ReverseDayHash_SampleData_ReturnsCorrectValue()
+        {
+            Assert.Equal(new DateTime(2019, 2, 10), BusinessDayCounter.ReverseDayHash(737099));
+            Assert.Equal(new DateTime(2019, 2, 1), BusinessDayCounter.ReverseDayHash(737090));
+            Assert.Equal(new DateTime(2019, 1, 1), BusinessDayCounter.ReverseDayHash(737059));
+        }
+
+        [Fact]
+        public void GetDay_SampleData_ReturnsCorrectValue()
+        {
+            Assert.Equal(0, BusinessDayCounter.GetDay(new DateTime(1, 1, 1)));
+            Assert.Equal(1, BusinessDayCounter.GetDay(new DateTime(1, 1, 2)));
+            Assert.Equal(3, BusinessDayCounter.GetDay(new DateTime(1, 2, 1)));
+            Assert.Equal(3, BusinessDayCounter.GetDay(new DateTime(1, 3, 1)));
+            Assert.Equal(1, BusinessDayCounter.GetDay(new DateTime(2, 1, 1)));
+            Assert.Equal(2, BusinessDayCounter.GetDay(new DateTime(3, 1, 1)));
+            Assert.Equal(5, BusinessDayCounter.GetDay(new DateTime(5, 1, 1)));
+            Assert.Equal(5, BusinessDayCounter.GetDay(new DateTime(11, 1, 1)));
+            Assert.Equal(5, BusinessDayCounter.GetDay(new DateTime(101, 1, 1)));
+            Assert.Equal(0, BusinessDayCounter.GetDay(new DateTime(401, 1, 1)));
+            Assert.Equal(0, BusinessDayCounter.GetDay(new DateTime(1601, 1, 1))); // Monday
+        }
+
+        [Fact]
+        public void GetDay_SampleData_1752EdgeCase_ReturnsCorrectValue()
+        {
+            Assert.Equal(5, BusinessDayCounter.GetDay(new DateTime(1752, 1, 1)));
+            Assert.Equal(1, BusinessDayCounter.GetDay(new DateTime(1752, 2, 1)));
+            Assert.Equal(2, BusinessDayCounter.GetDay(new DateTime(1752, 3, 1)));
+            Assert.Equal(5, BusinessDayCounter.GetDay(new DateTime(1752, 4, 1)));
+            Assert.Equal(0, BusinessDayCounter.GetDay(new DateTime(1752, 5, 1)));
+            Assert.Equal(3, BusinessDayCounter.GetDay(new DateTime(1752, 6, 1)));
+            Assert.Equal(5, BusinessDayCounter.GetDay(new DateTime(1752, 7, 1)));
+            Assert.Equal(1, BusinessDayCounter.GetDay(new DateTime(1752, 8, 1)));
+
+            // Why Our Calendars Skipped 11 Days in 1752
+            //http://mentalfloss.com/article/51370/why-our-calendars-skipped-11-days-1752
+            // Old 0 = Friday
+            // New 0 = Monday
+            Assert.Equal(4, BusinessDayCounter.GetDay(new DateTime(1752, 9, 1))); // Tuesday
+            Assert.Equal(5, BusinessDayCounter.GetDay(new DateTime(1752, 9, 2))); // Wednesday
+            Assert.Equal(3, BusinessDayCounter.GetDay(new DateTime(1752, 9, 14))); // Thursday
+
+            Assert.Equal(6, BusinessDayCounter.GetDay(new DateTime(1752, 10, 1)));
+            Assert.Equal(2, BusinessDayCounter.GetDay(new DateTime(1752, 11, 1)));
+            Assert.Equal(4, BusinessDayCounter.GetDay(new DateTime(1752, 12, 1)));
+            Assert.Equal(6, BusinessDayCounter.GetDay(new DateTime(1752, 12, 31)));
+            Assert.Equal(0, BusinessDayCounter.GetDay(new DateTime(1753, 1, 1)));
+        }
+
+        [Fact]
+        public void GetDay_SampleData_ModernDates_ReturnsCorrectValue()
+        {
+            Assert.Equal(5, BusinessDayCounter.GetDay(new DateTime(1788, 1, 26))); // Saturday
+            Assert.Equal(3, BusinessDayCounter.GetDay(new DateTime(1801, 1, 1))); // Thursday
+            Assert.Equal(1, BusinessDayCounter.GetDay(new DateTime(1901, 1, 1))); // Tuesday
+            Assert.Equal(0, BusinessDayCounter.GetDay(new DateTime(2001, 1, 1))); // Monday
+            Assert.Equal(5, BusinessDayCounter.GetDay(new DateTime(2005, 1, 1)));
+            Assert.Equal(3, BusinessDayCounter.GetDay(new DateTime(2009, 1, 1)));
+            Assert.Equal(1, BusinessDayCounter.GetDay(new DateTime(2013, 1, 1)));
+            Assert.Equal(6, BusinessDayCounter.GetDay(new DateTime(2017, 1, 1)));
+            Assert.Equal(1, BusinessDayCounter.GetDay(new DateTime(2017, 10, 10))); // Tuesday
+        }
+
+        [Fact]
+        public void GetDay_SampleData_2019Test_ReturnsCorrectValue()
+        {
+            Assert.Equal(1, BusinessDayCounter.GetDay(new DateTime(2019, 1, 1)));
+            Assert.Equal(4, BusinessDayCounter.GetDay(new DateTime(2019, 2, 1)));
+            Assert.Equal(6, BusinessDayCounter.GetDay(new DateTime(2019, 2, 10))); // Sunday
         }
     }
 }
